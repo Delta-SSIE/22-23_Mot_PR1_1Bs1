23268d40eb04abc755ec953042181b21e86c9d20
Jan Mottl <mottja@delta-skola.cz>
2022-09-16 11:21:06 +0200

    push2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int count = 10;
+            for (int i = 0; i < count; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ Uvod/Uvod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uvod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tohle je komentář - následující řádek kódu vypíše "Ahoj světě!" do konzole
+            Console.WriteLine("Ahoja světě!");
+            /*
+             * Tohle 
+             * je komentář 
+             * na více řádků
+             */
+            Console.WriteLine("Ahoj Evropo!");
+
+        //Write vypisuje do jednoho řádku
+            Console.Write("Ahoj");
+            Console.WriteLine(" Jane");
+
+            //Výpis čísla bez uvozovek
+            Console.WriteLine(3);
+        }
+    }
+}
