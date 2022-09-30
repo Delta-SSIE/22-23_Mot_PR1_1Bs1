e527d410eb87f2c408f6447ec3f73a8d864af528
Jan Mottl <mottja@delta-skola.cz>
2022-09-30 12:55:58 +0200

    logický operátor AND

---
+++ Podmínky s logickým operátorem/Podmínky s logickým operátorem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Podmínky_s_logickým_operátorem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string heslo1 = "pes";
+            string heslo2 = "letadlo";
+
+            Console.Write("Nyní zadá uživatel 1 své heslo: ");
+            string zadheslo1 = Console.ReadLine();
+            Console.Write("Nyní zadá uživatel 2 své heslo: ");
+            string zadheslo2 = Console.ReadLine();
+            
+            //Použití logického opearátoru AND
+            if (heslo1 == zadheslo1 && heslo2 == zadheslo2)
+            {
+                Console.WriteLine("Autodestrukce spuštěna");
+            }
+
+            //To stejné bez AND
+            if (heslo1 == zadheslo1)
+            {
+               if (heslo2 == zadheslo2)
+                {
+                    Console.WriteLine("Autodestrukce spuštěna");
+                }
+            }
+        }
+    }
+}
