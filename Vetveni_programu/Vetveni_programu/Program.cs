5e6268e67150d095a49e624e7a1d66cd60570339
Jan Mottl <mottja@delta-skola.cz>
2022-09-30 11:58:36 +0200

    Vetvení 1

---
+++ Vetveni_programu/Vetveni_programu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Vetveni_programu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            
+            // Boolean datový typ 
+            bool vyssi = x > y;
+            Console.WriteLine(vyssi);
+
+            //První použití proměnné IF
+            string heslo = "heslo123";
+            string zadaneheslo = Console.ReadLine();
+            if (zadaneheslo == heslo) 
+            {
+                //tohle se spustí pokud podmínka bude TRUE
+                Console.WriteLine("Jste přihlášen!");
+            }
+            Console.WriteLine("Konec programu!");
+
+
+        
+        }
+    }
+}
