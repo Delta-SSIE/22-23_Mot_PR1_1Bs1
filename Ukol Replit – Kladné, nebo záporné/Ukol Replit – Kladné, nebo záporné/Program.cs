72d0590571f5a23a5cfe70d7900c472f2b24e9ee
Jan Mottl <mottja@delta-skola.cz>
2022-09-30 12:45:02 +0200

    Vyšší číslo

---
+++ Ukol Replit – Kladné, nebo záporné/Ukol Replit – Kladné, nebo záporné/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ukol_Replit___Kladné__nebo_záporné
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Napiš mi číslo ");
+            int cislo = int.Parse(Console.ReadLine());
+
+            if (cislo > 0)
+            {
+                Console.WriteLine("Číslo je kladné!");
+            } else if (cislo == 0)
+            {
+                Console.WriteLine("Číslo je nula!");
+            } else
+            {
+                Console.WriteLine("Čislo je záporné!");
+            }
+        }
+    }
+}
