828ddae86a01f35d7b4cac0e648b6248d703b138
Jan Mottl <mottja@delta-skola.cz>
2022-10-14 11:50:17 +0200

    up

---
+++ 02_Nahodna_cisla.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Nahod_cisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Založení generátoru (v kódu je právě jednou!)
+            Random Genrand = new Random();
+
+            //Jak generovat? – Genrand.Next(dolní mez, horní mez-1)
+            Console.WriteLine(Genrand.Next(1, 11));
+            Console.WriteLine(Genrand.Next(1, 11));
+            Console.WriteLine(Genrand.Next(1, 11));
+            Console.WriteLine(Genrand.Next(1, 11));
+            Console.WriteLine(Genrand.Next(1, 11));
+            Console.WriteLine(Genrand.Next(1, 11));
+            Console.WriteLine(Genrand.Next(1, 11));
+
+            //Specialita – Genrand.NextDouble()
+            Console.WriteLine(Genrand.NextDouble());
+            Console.WriteLine(Genrand.NextDouble());
+            Console.WriteLine(Genrand.NextDouble());
+            Console.WriteLine(Genrand.NextDouble());
+        }
+    }
+}
