31c06a97e41623d510b1e3d7eaeb86c68e076ca8
Jan Mottl <mottja@delta-skola.cz>
2022-10-14 12:02:30 +0200

    update

---
+++ 02_Ukolrep_hra_kostky.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hra_kostky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Kostka = new Random();
+            int hodpocitace = Kostka.Next(1, 7);
+
+            Console.WriteLine("hoď kostkou");
+            Console.ReadKey();
+            int hodhrace = Kostka.Next(1, 7);
+
+            if (hodhrace == hodpocitace)
+            {
+                Console.WriteLine($"{hodpocitace}:{hodhrace} (Remíza)");
+            } else if (hodhrace > hodpocitace)
+            {
+                Console.WriteLine($"{hodpocitace}:{hodhrace} (Vyhrál jsi)");
+            } else
+            {
+                Console.WriteLine($"{hodpocitace}:{hodhrace} (Prohrál jsi)");
+            }
+        }
+    }
+}
