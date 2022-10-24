9be47603f4fdb74a1c5482d6d5097c4886c11829
Jan Mottl <mottja@delta-skola.cz>
2022-10-24 12:01:08 +0200

    up

---
+++ 03_CyklWhile_heslo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace While_heslo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Napiš heslo: ");
+            string zadheslo = Console.ReadLine();
+            string heslo = "ahoj123";
+
+            // Začátek cyklu, ten se bude opakovat, dokud se zadheslo nebude shodovat s heslem.
+            while (zadheslo != heslo)
+            {
+                //Console.Clear slouží k vyčištění konzole
+                Console.Clear();
+                Console.WriteLine("Zadali jste nesprávné heslo!");
+                Console.Write("Zadej heslo znovu: ");
+                zadheslo = Console.ReadLine();
+            }
+
+            Console.WriteLine("Gratlujeme, úspěšně jste se přihlásil do systému!");
+        }
+    }
+}
---
+++ 03_CyklWhile_pocetpozdravu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace While_vypis_dle_zadaneho_poctu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kolikrát tě má program pozdravit: ");
+            int pocetpozdrav = int.Parse(Console.ReadLine());
+            //Pomocná proměnná na počítání průchodu cyklu.
+            int i = 1;
+
+            while (i <= pocetpozdrav)
+            {
+                Console.WriteLine("Hello World!");
+                //zváýšení proměnné o 1
+                i++;
+            }
+        }
+    }
+}
