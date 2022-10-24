4f8351791ddd005deb4478363f7d99b6daa361c0
Jan Mottl <mottja@delta-skola.cz>
2022-10-24 13:01:22 +0200

    up

---
+++ 03_Ukolrep_Malanasob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace While_malanasob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int zaklad = 5;
+            // Pomocná proměnná na kontrolu průchodu cyklu.
+            int i = 1;
+
+            //Cyklus se opakuje, dokud i není větší než 10 (tzn. aspoň 11)
+            while (i <= 10)
+            {
+                Console.WriteLine($"{i}x{zaklad} = {i*zaklad}");
+                //Zkracený zápis pro i = i + 1;
+                i++;
+            }
+        }
+    }
+}
---
+++ 03_Ukolrep_ScitaniCisel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace While_scitanicisel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadávej čísla, já je budu sčítat. Vložení 0 program ukončí.");
+            int vstup = 2;
+            int soucet = 0;
+            while (vstup != 0)
+            {
+                vstup = int.Parse(Console.ReadLine());
+                //Zkracený zápis pro soucet = soucet + vstup
+                soucet += vstup;
+            }
+
+            Console.WriteLine($"Součet čísel je {soucet}");
+        }
+    }
+}
