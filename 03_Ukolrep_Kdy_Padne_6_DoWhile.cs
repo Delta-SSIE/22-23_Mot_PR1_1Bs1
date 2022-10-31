709d6f2515330a508b4e2d70e0b7d1dd9356a4e5
Jan Mottl <mottja@delta-skola.cz>
2022-10-31 12:13:34 +0100

    ok

---
+++ 03_Ukolrep_Kdy_Padne_6_DoWhile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace While_Sestka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vítej v programu, který hází kostkou než hodí šestku!");
+            string konec = "";
+            while (konec != "N")
+            {
+                Random kostka = new Random();
+                int hozenecislo = 0;
+                int hod = 0;
+                do
+                {
+                    hozenecislo = kostka.Next(1, 7);
+                    hod++;
+                } while (hozenecislo != 6);
+
+                Console.WriteLine($"Šetsku jsem hodil až na {hod} hod kostkou");
+                Console.Write("Chceš hrát znovu? hru ukončíš vložením \"N\"");
+                konec = Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ 03_Ukolrep_Soucet_cisel_interval.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace While_Interval
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("zadej jeden kraj intervalu:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("zadej druhý kraj intervalu:");
+            int b = int.Parse(Console.ReadLine());
+
+            int max = Math.Max(a, b);
+            int min = Math.Min(a, b);
+
+            int sum = 0;
+            min++;
+
+            while (min != max)
+            {
+                sum += min;
+                min++;
+            }
+
+            Console.WriteLine($"Součet čísel v intervalu je {sum}");
+
+        }
+    }
+}
