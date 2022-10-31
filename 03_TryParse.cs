1c0d694de79b8e7e617629a183ebfd96f6d362ec
Jan Mottl <mottja@delta-skola.cz>
2022-10-31 12:52:57 +0100

    ok

---
+++ 03_TryParse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+
+namespace Tryparse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vstup = Console.ReadLine();
+            int cislo;
+            Console.WriteLine(int.TryParse(vstup, out cislo));
+
+
+            string a;
+            do
+            {
+                Console.WriteLine("Zadej číslo: ");
+                a = Console.ReadLine();
+            } while (!int.TryParse(a, out cislo));
+
+        }
+    }
+}
---
+++ 03_Ukolrep_Maximum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace While_Maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo = 0;
+            int max = 0;
+            do
+            {
+                Console.Write("Zadej číslo (ukončení 0, či menší) ");
+                cislo = int.Parse(Console.ReadLine());
+                max = Math.Max(max, cislo);
+            } while (cislo > 0);
+            if (max == 0)
+            {
+                Console.Write("Nezadal si žádné číslo!");
+            } else
+            {
+                Console.WriteLine($"Největší číslo ze zadaných je {max}");
+            }
+            
+        }
+    }
+}
