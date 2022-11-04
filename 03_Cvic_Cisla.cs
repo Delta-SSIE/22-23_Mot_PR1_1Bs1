61e918f9b5738a0253d2e90588b48e193981b81b
Jan Mottl <mottja@delta-skola.cz>
2022-11-04 13:05:00 +0100

    ok

---
+++ 03_Cvic_Cisla.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace cisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int min = 9999999;
+            int souclich = 0;
+            int soucsud = 0;
+            int cislo;
+
+            do
+            {
+                Console.Write("Zadej číslo (0 pro konec) ");
+                cislo = int.Parse(Console.ReadLine());
+                if (cislo % 2 == 0)
+                {
+                    soucsud += cislo;
+                }
+                else
+                {
+                    souclich += cislo;
+                }
+                max = Math.Max(max, cislo);
+                if (cislo != 0) { 
+                min = Math.Min(min, cislo);
+                }
+            } while (cislo != 0);
+
+            Console.WriteLine($"Součet sudých čísel je {soucsud} \nSoučet lichých čísel je {souclich} \nNejvětší číslo je {max} \nNejmenší číslo je {min}");
+        }
+    }
+}
---
+++ 03_Cvic_Nakup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Nakup_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vítej v programu Nákupní seznam!");
+            int pocetpol = 0;
+            double cena = 0;
+            string zbozi;
+
+            do
+            {
+                Console.WriteLine("Chceš koupit rohlík, housku, či chleba? (zádávej r/h/ch)");
+                zbozi = Console.ReadLine();
+                if (zbozi == "r")
+                {
+                    // cena = cena + 3.5;
+                    cena += 3.5;
+                    pocetpol++;
+                }
+                if (zbozi == "h")
+                {
+                    cena += 5.5;
+                    pocetpol++;
+                }
+                if (zbozi == "ch")
+                {
+                    cena += 10;
+                    pocetpol++;
+                }
+            } while (zbozi != "0" && pocetpol < 10);
+
+            Console.WriteLine($"Počet položek na seznamu je {pocetpol} a celková cena je {cena}");
+        }
+    }
+}
