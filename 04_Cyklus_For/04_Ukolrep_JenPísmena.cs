2dea6d3a260e5d277d8986cd52999cf1cabc066b
Jan Mottl <mottja@delta-skola.cz>
2022-11-21 13:04:44 +0100

    ok

---
+++ 04_Cyklus_For/04_Ukolrep_JenPísmena.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Nechte_Jen_Pismena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Jean-Luc, you should inform no. 1 and then go and see what happened at 8:30PM!";
+            string final = "";
+            int a = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                //Konvertuji znak na číslo
+                a = (int)Convert.ToChar(text[i]);
+                //Zjištuji, zda-li dané číslo je v rozmezí, kdy se mu jednat o znak
+                if((a < 91 && a > 64) || (a < 123 && a > 96))
+                {
+                    //Znak přidávám k finálnímu stringu
+                    final += text[i];
+                }
+            }
+            Console.WriteLine(final);
+        }
+    }
+}
---
+++ 04_Cyklus_For/04_Ukolrep_KazdyDruhyZnak.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kazdy_Druhy_Znak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "zadřeš on i co onsemnaí";
+
+            for (int i = 0; i < text.Length; i+=2)
+            {
+                Console.Write(text[i]);
+            }
+        }
+    }
+}
