ba0e20986c0dd250a76b57015b2271c0f3c60743
Jan Mottl <mottja@delta-skola.cz>
2022-11-25 12:26:05 +0100

    ok

---
+++ 04_Cyklus_For/04_Ukolrep_PoSchodoch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Schody
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolik pater chceš?");
+            int patra = int.Parse(Console.ReadLine());
+
+            //Tenhle cyklus řeší skoky po řádcích
+            for (int i = 0; i < patra; i++)
+            {
+                //Tenhle cyklus řeší naplnění jednotlivých řádků
+                for (int a = 0; a <= i; a++)
+                {
+                    Console.Write("#");
+                }
+                //Tady se řeší, aby se vždy na konci řádku došlo k zalomení a cyklus v příštím průchodu začal na řádku novém!
+                Console.WriteLine("");
+            }
+        }
+    }
+}
