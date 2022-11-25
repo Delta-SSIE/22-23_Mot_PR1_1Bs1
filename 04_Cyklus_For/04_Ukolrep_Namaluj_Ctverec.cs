0a462cf0afb3e3f867e400cdfbdabfc099476d0e
Jan Mottl <mottja@delta-skola.cz>
2022-11-25 12:50:33 +0100

    ok

---
+++ 04_Cyklus_For/04_Ukolrep_Namaluj_Ctverec.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ctverec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej délku strany a: ");
+            int a = int.Parse(Console.ReadLine());
+            
+            //Cyklus řeší skoky po řádcích
+            for (int i = 0; i < a; i++)
+            {
+                //Cyklus řeší naplnění řádků #
+                for (int x = 0; x < a; x++)
+                {
+                    Console.Write("#");
+                }
+                //Zalomení řádku, aby další průchod začal na novém řádku!
+                Console.WriteLine();
+            }
+        }
+    }
+}
