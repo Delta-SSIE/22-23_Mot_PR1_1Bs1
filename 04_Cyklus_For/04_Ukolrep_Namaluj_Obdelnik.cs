ae8fd3d0a69531f36efeeab27ad11d913a398da0
Jan Mottl <mottja@delta-skola.cz>
2022-11-25 13:04:09 +0100

    ok

---
+++ 04_Cyklus_For/04_Ukolrep_Namaluj_Obdelnik.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Obdelnik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej stranu a, neboli šířku obdelníku ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Zadej stranu b, neboli výšku obdelníku ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            //Cyklus řeší výšku obdelníku
+            for (int i = 0; i < b; i++)
+            {
+                //Cyklus řeší šířku obdelníku
+                for (int x = 0; x < a; x++)
+                {
+                    Console.Write("#");
+                }
+                //Zalomení řádku
+                Console.WriteLine("");
+            }
+        }
+    }
+}
