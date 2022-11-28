5f506148512ce0b43a9e99a78895a16d5f3a54bc
Jan Mottl <mottja@delta-skola.cz>
2022-11-28 13:02:25 +0100

    ok

---
+++ 04_Cyklus_For/04_Ukolrep_Trojuhlenik_p.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Troj_p
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int strana = 10;
+
+            for (int i = 0; i < strana; i++)
+            {
+                for (int x = i; x < strana; x++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
