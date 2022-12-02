e1683de40ae90e119ad2ce4b8e66d272371439f9
Jan Mottl <mottja@delta-skola.cz>
2022-12-02 12:13:03 +0100

    up

---
+++ 04_Cyklus_For/04_Ukolrep_Trojuhleník_d.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace trojuhelnik_d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int strana = 10;
+
+            // Vnější cyklus, který řeší řádky
+            for (int i = strana; i > 0; i--)
+            {
+                //Vnitřní cyklus, který řeší naplňování řádků
+                for (int x = 1; x < strana+1; x++)
+                {
+                    //Podmínka, která řeší, jestli následující znak je mezera, či hash
+                    if (i > x)
+                    {
+                        Console.Write(" ");
+                    } else
+                    {
+                        Console.Write("#");
+                    }
+                }
+                //Odskočení o řádek níže!
+                Console.WriteLine();
+            }
+        }
+    }
+}
