2bc68b0f1b49d0fdf692bc29cd860ce2409f9418
Jan Mottl <mottja@delta-skola.cz>
2022-12-09 12:39:44 +0100

    ok

---
+++ 05_Pole/05_Ukolrep_Podmineny_soucet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Podmineny_soucet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = { 25, -56, -88, 5, 10, 65, 584, -8, -120, 18 };
+            int soucet = 0;
+            foreach (int i in cisla)
+            {
+                if (i <= 10 && i >= -10)
+                {
+                    soucet += i;
+                }
+            }
+
+            Console.WriteLine($"Součet položek mezi hodnotami -10 a 10 je {soucet}");
+
+        }
+    }
+}
