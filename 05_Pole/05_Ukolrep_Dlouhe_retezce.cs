a6ed71003644d6c84aa6b6178dc427c4ab44fec2
Jan Mottl <mottja@delta-skola.cz>
2022-12-09 12:59:54 +0100

    ok

---
+++ 05_Pole/05_Ukolrep_Dlouhe_retezce.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Dlouhe_retezce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bankaslov = new string[5];
+
+            for (int i = 0; i < bankaslov.Length; i++)
+            {
+                Console.WriteLine($"Zadej {i+1}.řetězec");
+                bankaslov[i] = Console.ReadLine();
+            }
+
+            Console.Clear();
+            Console.WriteLine("Nyní se vypíšou slova, která mají 6, či více znaků:");
+            foreach (string i in bankaslov)
+            {
+                if(i.Length >= 6)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
