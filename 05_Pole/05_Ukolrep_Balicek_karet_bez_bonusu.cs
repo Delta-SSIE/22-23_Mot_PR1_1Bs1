f9e3c7f538b771053b3765040137a50a2a82228c
Jan Mottl <mottja@delta-skola.cz>
2022-12-12 12:59:38 +0100

    ok

---
+++ 05_Pole/05_Ukolrep_Balicek_karet_bez_bonusu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace balicek_karet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] barva = { "listy", "kule", "žaludy", "srdce" };
+            string[] vyska = { "sedma", "osma", "devět", "deset", "spodek", "svršek", "král", "eso" };
+            string[] balicek = new string[32];
+            int index = 0;
+
+            //Foreach pro procházení po barvách (vnější cyklus)
+            foreach (string b in barva)
+            {
+                //Foreach pro procházení po výškách (vnitřní cyklus)
+                foreach (string v in vyska)
+                {
+                    //Zapsaní do třetího pole
+                    balicek[index] = b + " - " + v;
+                    //zvyšení indexu por třetí pole
+                    index++;
+                }
+
+            }
+
+            //Výpis pole balíček pomocí foreach
+            foreach (string karta in balicek)
+            {
+                Console.WriteLine(karta);
+            }
+        }
+    }
+}
