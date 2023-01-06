c3e74cc616b7ae18bde9dfd0dec55446f7c825c4
Jan Mottl <mottja@delta-skola.cz>
2023-01-06 12:14:31 +0100

    ok

---
+++ 05_Pole/05_Ukolrep_Histogram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Histogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] hodnoty = { 1, 3, 2, 4, 7, 12, 4 };
+
+            //Cyklus na procházení pole
+            for (int i = 0; i < hodnoty.Length; i++)
+            {
+                //Vypis začátku řádku, neboli index a dvojtečka.
+                Console.Write($"{i}:");
+                //Cyklus, který se opakuje podle toho jak vysoká je hodnota na daném indexu.
+                for (int x = 0; x < hodnoty[i]; x++)
+                {
+                    //Při každém průchodu se vypíše jeden hash!
+                    Console.Write("#");
+                }
+                //Zalomení řádku při přechodu na další index.
+                Console.WriteLine();
+            }
+        }
+    }
+}
