f94356a8ec2dbf2fb870a2952d82e50d35c016f4
Jan Mottl <mottja@delta-skola.cz>
2023-01-13 13:02:52 +0100

    ok

---
+++ 06_Podprogramy/06_Zaklad_metody.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace metody_zaklad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Volání metody s argumenty
+            Mojemetoda("Honzo!");
+            Mojemetoda("Vaáclave!");
+            Mojemetoda("Vítku!");
+            Mojemetoda("Filipe!");
+
+            //Volání metody s dvěma argumenty
+            Scitani(5, 5);
+            Scitani(10, 15);
+
+            //Volání metody s předepsaným parametrem
+            Zeme("Irsko");
+            Zeme();
+
+            //Volání metody s návratovou hodnotou
+            Console.WriteLine(Nasobeni(5, 5));
+            int x = Nasobeni(20, 40);
+            Console.WriteLine(x);
+
+            //Volání metody bez parametru s návratovou hodnotou
+            int y = Parsuj();
+            Console.WriteLine(y);
+
+        }
+
+        //Moje nová metoda (podprogram) s parametrem textovým (jmeno)
+        static void Mojemetoda(string jmeno)
+        {
+            Console.WriteLine($"Ahoj {jmeno}!");
+        }
+
+        //Metoda s dvěma parametry
+        static void Scitani(int x, int y)
+        {
+            Console.WriteLine($"Součet hodnot je {x+y}");
+        }
+
+        //Metoda s předepsaným parametrem
+        static void Zeme(string zem = "Česko")
+        {
+            Console.WriteLine("Jsem z " + zem);
+        }
+
+        //Metoda s return (návratovou hodnotou)
+        static int Nasobeni (int x, int y)
+        {
+            return x * y;
+        }
+
+        //Metoda bez parametru, co vrací číslo!
+        static int Parsuj ()
+        {
+            string a;
+            int cislo;
+            do
+            {
+                Console.WriteLine("Zadej číslo: ");
+                a = Console.ReadLine();
+            } while (!int.TryParse(a, out cislo));
+            return cislo;
+        }
+    }
+}
