ad505f5ccb3839327799d9751f1038c2c1eb46d6
Jan Mottl <mottja@delta-skola.cz>
2023-01-20 12:29:51 +0100

    ok

---
+++ 06_Podprogramy/06_Ukolrep_Delsi_slovo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace delsi_slovo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej první slovo: ");
+            string slovo1 = Console.ReadLine();
+
+            Console.Write("Zadej druhé slovo: ");
+            string slovo2 = Console.ReadLine();
+
+            string delsiSlovo = Delsislovo(slovo1, slovo2);
+            Console.WriteLine($"Když porovnáme {slovo1} a {slovo2}, pak delší je {delsiSlovo}.");
+        }
+
+        static string Delsislovo(string s1, string s2)
+        {
+            if (s1.Length > s2.Length)
+            {
+                return s1;
+            } else if (s2.Length > s1.Length)
+            {
+                return s2;
+            } else
+            {
+                return "žádné. Slova jsou stejně dlouhá";
+            }
+        }
+    }
+}
