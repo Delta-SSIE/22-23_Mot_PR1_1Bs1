e131ff908d9d822cddf8ed752e0197f9c73d4738
Jan Mottl <mottja@delta-skola.cz>
2023-01-20 12:42:30 +0100

    ok

---
+++ 06_Podprogramy/06_Ukolrep_Nejvetsi_cislo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace nejvetsi_cislo_met
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cislo1 = 1.3;
+            double cislo2 = 4.6;
+            double cislo3 = -3.3;
+            double maximum = Nejvetsi(cislo1, cislo2, cislo3);
+            Console.WriteLine($"Největší ze zadaných čísel je {maximum}.");
+        }
+
+        static double Nejvetsi(double x, double y, double z)
+        {
+            double[] polecisel = { x, y, z };
+            return polecisel.Max();
+        }
+    }
+}
