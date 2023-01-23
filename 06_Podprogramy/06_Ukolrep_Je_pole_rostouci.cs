2949729b819026e5904e98f8a692170974f68524
Jan Mottl <mottja@delta-skola.cz>
2023-01-23 11:53:58 +0100

    ok

---
+++ 06_Podprogramy/06_Ukolrep_Je_pole_rostouci.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Je_pole_rostouci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla1 = { -5, -3, 1, 7, 50 };
+            int[] cisla2 = { -5, 1, -3, 7, 50 };
+            int[] cisla3 = { -5, -3, 1, 1, 7, 50 };
+
+            //je
+            Console.WriteLine("První pole {0} rostoucí.", JeRostouci(cisla1) ? "je" : "není");
+
+            //není
+            Console.WriteLine("Druhé pole {0} rostoucí.", JeRostouci(cisla2) ? "je" : "není");
+
+            //není
+            Console.WriteLine("Třetí pole {0} rostoucí.", JeRostouci(cisla3) ? "je" : "není");
+        }
+
+        static bool JeRostouci(int[] pole)
+        {
+            for (int i = 0; i < pole.Length; i++)
+            {
+                if (i == pole.Length-1)
+                {
+
+                }
+                else
+                {
+                    if (pole[i] >= pole[i + 1])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+}
+
---
+++ 06_Podprogramy/06_Ukolrep_Pocet_celych_cisel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace pole_celych_cisel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] cisla = { 1.3, 1.7, 2, 3.1, 0.1, -1 };
+            //obsahuje 2 celá čísla, 2 a -1
+
+            Console.WriteLine(
+                "Pole  [{0}] obsahuje {1} celých čísel",
+                string.Join("; ", cisla),
+                PocetCelych(cisla)
+                );
+        }
+
+        static int PocetCelych(double[] pole)
+        {
+            int pocet = 0;
+            foreach (double a in pole)
+            {
+                if (a % 1 == 0)
+                {
+                    pocet++;
+                }
+            }
+            return pocet;
+        }
+    }
+}
