d82fac59bdfa6df551c2f29dda120cbe6d9588f7
Jan Mottl <mottja@delta-skola.cz>
2023-02-06 12:39:44 +0100

    ok

---
+++ 07_Vicerozmerne_pole/07_Ukolrep_Kresleni.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace kresleni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[,] mapa =
+          {
+            {  true, false, false,  true, false, false,  true},
+            { false,  true, false,  true, false,  true, false},
+            { false, false,  true,  true,  true, false, false},
+            { false, false, false,  true, false, false, false},
+        };
+
+        VykresliPole(mapa);
+            VykresliPole(mapa, "X", "-");
+            VykresliPole(mapa, "1", "0");
+
+        }
+
+        static void VykresliPole(bool[,] kresleni, string znak = "#", string prazdnemisto = " ")
+        {
+            //Vnější cyklus chodí po jednotlivých řádcích 2D pole (index řádků je tedy i)
+            for (int i = 0; i < kresleni.GetLength(0); i++)
+            {
+                //Vnitřní cyklus chodí po sloupcích 2D pole (index sloupce je tedy j)
+                for (int j = 0; j < kresleni.GetLength(1); j++)
+                {
+                    if (kresleni[i, j])
+                    {
+                        Console.Write(znak);
+                    } else
+                    {
+                        Console.Write(prazdnemisto);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
