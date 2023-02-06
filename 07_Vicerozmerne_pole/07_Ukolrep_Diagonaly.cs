2be9fefb696bb0828aa0c3f417272fd61482bd33
Jan Mottl <mottja@delta-skola.cz>
2023-02-06 12:23:39 +0100

    ok

---
+++ 07_Vicerozmerne_pole/07_Ukolrep_Diagonaly.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace diagonaly
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[,] diagonaly = { {"#"," "," ", " ", "#" },{" ", "#", " ", "#", " " },{" ", " ", "#", " ", " "}, { " ", "#", " ", "#", " " } , { "#", " ", " ", " ", "#" } };
+            Vypis2DPole(diagonaly);
+
+        }
+        public static void Vypis2DPole(string[,] pole)
+        {
+            for (int y = 0; y < pole.GetLength(0); y++)
+            {
+                for (int x = 0; x < pole.GetLength(1); x++)
+                {
+                    Console.Write(pole[y, x]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
