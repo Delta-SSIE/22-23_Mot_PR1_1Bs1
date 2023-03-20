c468fa749197fe8ae3fb4feae19a721e41ea3626
Jan Mottl <mottja@delta-skola.cz>
2023-03-20 11:57:52 +0100

    up

---
+++ 09_OOP/Enum_Zaklad.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Enum_zaklad
+{
+    enum Dny
+    {
+        Pondeli,//0
+        Utery,  //1
+        Streda, //2
+        Ctvrtek,//3
+        Patek,  //4
+        Sobota, //5
+        Nedele  //6
+    }
+
+    enum Mesice
+    {
+        Leden,  //0
+        Unor=6, //6
+        Brezen  //7
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklarace proměnné z enum
+            Dny dnesden = Dny.Pondeli;
+            //Výpis proměnné
+            Console.WriteLine(dnesden);
+            //Posun o jedna dopředu v enum
+            dnesden++;
+            Console.WriteLine(dnesden);
+            //Posun o jedna dozadu v enum
+            dnesden--;
+            Console.WriteLine(dnesden);
+
+            //Index z enum
+            int indexenum = (int)Dny.Streda;
+            Console.WriteLine(indexenum);
+
+            //Index z posunutého pole
+            int indexmesic = (int)Mesice.Brezen;
+            Console.WriteLine(indexmesic);
+
+        }
+    }
+}
---
+++ 09_OOP/Vyjimky_Zaklad.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Vyjimky_Zaklad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vyjimky
+            Console.WriteLine("Zadej číslo");
+            //Try je kus kodu, který se pokusí provést
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+            }
+            //Catch se spustí pokud nastane chyba a kód se neprovede správně. Zároveň program nespadne
+            catch (Exception e)
+            {
+                //e.Message vypíše chybovou hlášku
+                Console.WriteLine(e.Message);
+            }
+
+            //Ukázka s vlastní chybovou hláškou
+            Console.WriteLine("Zadej číslo");
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Stala chyba při převodu na formát int.");
+            }
+        }
+    }
+}
