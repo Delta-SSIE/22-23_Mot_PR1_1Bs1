52f4e05618c40a1f9d3efad43331c15fe37e5b2a
Jan Mottl <mottja@delta-skola.cz>
2023-03-27 12:44:16 +0200

    up

---
+++ 09_OOP/09_Konstruktory_Zaklad/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konstruktory_Zaklad
+{
+    internal class Animal
+    {
+        //Fields
+        public int Nohy;
+        public string Jmeno;
+
+        //Konstruktor classy Animal
+        public Animal(int legs, string name)
+        {
+            Nohy = legs;
+            Jmeno = name;
+        }
+
+        public void PredstavSe()
+        {
+            Console.WriteLine($"Ahoj, já jsem {Jmeno} a mám {Nohy} nohy!");
+        }
+    }
+
+    
+}
---
+++ 09_OOP/09_Konstruktory_Zaklad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace Konstruktory_Zaklad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tvorba objektů s argumenty pro konstruktor
+            Animal leopard = new Animal(4, "Josse");
+            Animal zirafa = new Animal(4, "Jane");
+            Animal ryba = new Animal(0, "Dave");
+
+            leopard.PredstavSe();
+            zirafa.PredstavSe();
+            ryba.PredstavSe();
+        }
+    }
+}
