de3c03548fe21b4002ec6701bfe2bf7333d677be
Jan Mottl <mottja@delta-skola.cz>
2023-03-27 11:57:16 +0200

    up

---
+++ 09_OOP/09_Ukolrep_Pes/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trida_pes
+{
+    internal class Pes
+    {
+        //Tvorba fields (položek) / vlastností dané třídy.
+        public string Jmeno = "Alik";
+        public int Nohy = 4;
+        public bool JeOckovany = false;
+
+        //metoda s návratovou hodnotou dané třídy!
+        public string PredstavSe()
+        {
+            string zprava = $"Já jsem pes, jmenuju se {Jmeno}, mám {Nohy} nohy a ";
+            if (JeOckovany == true)
+            {
+                zprava = zprava + "jsem očkovaný";
+            } else
+            {
+                zprava = zprava + "jsem neočkovaný";
+            }
+            return zprava;
+        }
+    }
+}
---
+++ 09_OOP/09_Ukolrep_Pes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace trida_pes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //tento kód není třeba měnit
+            
+            //Vytváří se nový objekt "lord" z třídy Pes
+            Pes lord = new Pes();
+            //Definujeme vlastnost (položka) Jmeno objektu lord.
+            lord.Jmeno = "Lord";
+            //Definujeme vlastnost (položka) Nohy objektu lord.
+            lord.Nohy = 4;
+            //Definujeme vlastnost (položka) JeOckovany objektu lord.
+            lord.JeOckovany = true;
+
+            //Vypís metody objektu lord
+            Console.WriteLine(lord.PredstavSe());
+
+            //Zde je stejný proces, ale u nového objektku jméno "podvratak"
+            Pes podvratak = new Pes();
+            podvratak.Jmeno = "Vořech";
+            podvratak.Nohy = 3;
+            podvratak.JeOckovany = false;
+
+            Console.WriteLine(podvratak.PredstavSe());
+
+            //Nový objekt
+            Pes alik = new Pes();
+            Console.WriteLine(alik.PredstavSe());
+
+        }
+    }
+}
