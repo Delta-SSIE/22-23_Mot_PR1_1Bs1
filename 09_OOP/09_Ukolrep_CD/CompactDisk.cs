04e7ed5b0468ade7f2160993f84321da310ef0d4
Jan Mottl <mottja@delta-skola.cz>
2023-03-27 12:13:49 +0200

    up

---
+++ 09_OOP/09_Ukolrep_CD/CompactDisk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CD_disk
+{
+    internal class CompactDisk
+    {
+        //Položky
+        public string Title;
+        public string Artist;
+        public int Year;
+        public string[] Tracks;
+
+        //Metodu
+        public string Description()
+        {
+            string zprava = $"\"{Title}\" by {Artist} was published at {Year}. It contains {Tracks.Length} tracks";
+            return zprava;
+        }
+    }
+}
---
+++ 09_OOP/09_Ukolrep_CD/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CD_disk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vytvořen nový objekt jménem "CD1"
+            CompactDisk CD1 = new CompactDisk();
+
+            //Nadefinovat jednotlivý fields pro obejkt CD1
+            CD1.Title = "Waterloo";
+            CD1.Artist = "ABBA";
+            CD1.Year = 1974;
+            CD1.Tracks =  new string[] {"Waterloo","Sitting in the Palmtree","King Kong Song","Hasta Mañana"};
+
+            //Vypíšu návratovou hodnotu
+            Console.WriteLine(CD1.Description());
+
+        }
+    }
+}
