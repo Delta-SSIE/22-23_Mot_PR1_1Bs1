1d2ff656b87cce4e7a935532faf20efea0bbb401
Jan Mottl <mottja@delta-skola.cz>
2023-04-03 12:30:41 +0200

    up

---
+++ 09_OOP/09_Ukolrep_Bankovni_ucet/BankAccount .cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ukolrep_Bankovni_ucet
+{
+    internal class BankAccount
+    {
+        //Veřejný (public) vlastnosti třídy BankAccount
+        public int AccountNumber;
+        public string AccountHolderName;
+        public int AccountBalance;
+        public string AccountType;
+        
+        //Konstruktor, který na začátku naplní vlastnosti nového objektu dané třídy hodnotami.
+        public BankAccount(int number, string name, int balance, string type)
+        {
+            AccountNumber = number;
+            AccountHolderName = name;
+            AccountBalance = balance;
+            AccountType = type;
+        }
+
+        //Veřejná metoda Deposit (vklad), který přičte k hodnotě AccountBalance zadanou hodnotu castka.
+        public void Deposit(int castka)
+        {
+            AccountBalance = AccountBalance + castka;
+            Console.WriteLine($"Váš nový zůstatek je {AccountBalance}");
+        }
+
+        //Veřejná metoda Witthdrawal(výběr), která odečte hodnotu castak od hodnoty ve vlastnosti AccountBalance.
+        public void Withdrawal (int castka)
+        {
+            //Kontrola, jestli má dost peněz pro výběr.
+            if (castka > AccountBalance)
+            {
+                Console.WriteLine("Nemáte dostatek peněz na účtu!");
+            } else
+            {
+                AccountBalance = AccountBalance - castka;
+                Console.WriteLine($"Váš nový zůstatek je {AccountBalance}");
+            }
+        }
+
+    }
+}
---
+++ 09_OOP/09_Ukolrep_Bankovni_ucet/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Ukolrep_Bankovni_ucet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tvorba objektu pomocí konstruktoru
+            BankAccount mujucet = new BankAccount(1, "Honza", 1000, "Studentsky");
+
+            //Nekonečná smyčka
+            while (true)
+            {
+                int volba = 0;
+                //Vyčištění konzole
+                Console.Clear();
+                //Výpis základních informací o účtu
+                Console.WriteLine($"Vítej {mujucet.AccountHolderName} s pořadovým číslem účtu {mujucet.AccountNumber}." +
+                    $" na vašem účtu zbývá {mujucet.AccountBalance} a typ účtu je {mujucet.AccountType}");
+                //Jednoduchý menu pro další kroky
+                Console.WriteLine("Pro výběr napiš 1\nPro vklad napiš 2");
+                //Řešení výjimky, pokud by zadal něco, co není číslo
+                try
+                {
+                    volba = int.Parse(Console.ReadLine());
+                } catch
+                {
+                    Console.WriteLine("Zadali jste špatně!");
+                    Console.ReadKey();
+                }
+                //Podmínka pro správu menu
+                if (volba == 1)
+                {
+                    //Dotaz na to kolik chce vybrat
+                    Console.WriteLine("Kolik chceš vybrat?");
+                    int castka = int.Parse(Console.ReadLine());
+                    //Zavolání metody Withdrawal
+                    mujucet.Withdrawal(castka);
+                    Console.ReadKey();
+                } else if (volba == 2)
+                {
+                    //Dotaz na to kolik chce vložit
+                    Console.WriteLine("Kolik chceš vložit?");
+                    int castka = int.Parse(Console.ReadLine());
+                    //Zavolání metody Deposit
+                    mujucet.Deposit(castka);
+                    Console.ReadKey();
+                }
+     
+            }
+        }
+    }
+}
