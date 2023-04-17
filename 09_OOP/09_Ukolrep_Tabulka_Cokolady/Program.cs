9629373e0cf37ca10b7159ca0300cc9b13b7fe36
Jan Mottl <mottja@delta-skola.cz>
2023-04-17 12:56:19 +0200

    up

---
+++ 09_OOP/09_Ukolrep_Tabulka_Cokolady/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tabulka_Cokolady
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabulka dobrota = new Tabulka();
+              //zde nastavte další vlastnosti
+            dobrota.Druh = "Mléčná";
+             dobrota.Znacka = "Kráááva";
+              dobrota.Hmotnost = 120;
+
+                while (dobrota.Hmotnost > 0)
+                {
+                    int puvodni = dobrota.Hmotnost;
+                    int odlomeno = dobrota.Odlom();
+                    int zbyde = dobrota.Hmotnost;
+
+                    Console.WriteLine($"Tabulka typu {dobrota.Druh} čokoláda měla hmotnost {puvodni} gramů, odlomili jsme {odlomeno} gramů a zbylo {zbyde} gramů.");
+                }
+
+                Tabulka macek = new Tabulka();
+                //macek.SetHmotnost(3000);
+                //až ověříte, že se správně testují hmotnosti a sami si doděláte jiné testy vlastností, kód si zakomentujte, ať nerozbíjí program
+            }
+        }
+}
---
+++ 09_OOP/09_Ukolrep_Tabulka_Cokolady/Tabulka.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabulka_Cokolady
+{
+    internal class Tabulka
+    {
+        private string znacka;
+        private int hmotnost;
+        private string druh;
+
+        public string Znacka
+        {
+            get { return znacka; }
+            set
+            {
+                znacka = value;
+            }
+        }
+
+        public int Hmotnost
+        {
+            get { return hmotnost; }
+            set
+            {
+                if (value > 1000)
+                {
+                    hmotnost = 1000;
+                }
+                else
+                {
+                    hmotnost = value;
+                }
+            }
+        }
+        public string Druh
+        {
+            get { return druh; }
+            set
+            {
+                if (value != "Hořká" || value != "Mléčná" || value != "Bílá")
+                {
+                    druh = "Mléčná";
+                }
+                else
+                {
+                    druh = value;
+                }
+            }
+        }
+
+        public int Odlom()
+        {
+            int odlomek;
+            if (Hmotnost > 10)
+            {
+                odlomek = Hmotnost / 2;
+                Hmotnost -= odlomek;
+            } else
+            {
+                odlomek = Hmotnost;
+                Hmotnost = 0;
+            }
+            return odlomek;
+        }
+         
+    }
+}
