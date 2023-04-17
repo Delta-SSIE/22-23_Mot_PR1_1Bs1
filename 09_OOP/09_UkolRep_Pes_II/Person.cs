904ea58781c2d24356b9dea3925237832f9c0f4c
Jan Mottl <mottja@delta-skola.cz>
2023-04-17 12:05:05 +0200

    up

---
+++ 09_OOP/09_UkolRep_Pes_II/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zaklad_Zapouzdreni
+{
+    internal class Person
+    {
+        private double vyska; //field
+
+        public double Vyska //properties
+        {
+            //Ovlivnuji výpis, aby se převedl z cm do m!
+            get { return vyska/100; }
+
+            //Ovlivnění zápisu do proměnné, pomocí podmínky!
+            set { 
+            if (value > 272)
+                {
+                    vyska = 0;
+                } else
+                {
+                    vyska = value;
+                }
+            }
+        }
+
+    }
+}
