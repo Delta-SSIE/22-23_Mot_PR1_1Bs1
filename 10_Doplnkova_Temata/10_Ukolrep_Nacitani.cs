8b4061af1db1443b2a64bcda2effe6b20a77b21a
Jan Mottl <mottja@delta-skola.cz>
2023-06-16 12:54:16 +0200

    ok

---
+++ 10_Doplnkova_Temata/10_Ukolrep_Nacitani.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = NactiCisla();
+            Console.WriteLine("Byla vložena čísla: " + String.Join(", ", cisla));
+        }
+
+        static int[] NactiCisla()
+        {
+            int cislo;
+            List<Int32> cisla = new List<Int32>();
+            while (true)
+            {
+                Console.Write("Napiš jedno čislo (celočiselné): ");
+                try
+                {
+                    cislo = int.Parse(Console.ReadLine());
+                    cisla.Add(cislo);
+                }
+                catch
+                {
+                    if (Console.ReadLine() == "q" || Console.ReadLine() == "Q")
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Chtěl jsem číslo!");
+                    Console.ReadKey();
+                }
+            }
+            return cisla.ToArray();
+        }
+    }
+}
