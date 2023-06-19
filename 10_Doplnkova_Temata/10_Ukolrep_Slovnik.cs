dff702f7191ef4260b1f4e4e66f7aae86c2ef84f
Jan Mottl <mottja@delta-skola.cz>
2023-06-19 12:40:04 +0200

    ok

---
+++ 10_Doplnkova_Temata/10_Ukolrep_Slovnik.cs
@@ -0,0 +1,44 @@
+﻿namespace Slovnik_cz_ang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Dictionary<string, string> slovnik = new Dictionary<string, string>();
+            while (true)
+            {
+                //Dotaz na termín a jeho překlad
+                Console.Write("Napiš české slovo: ");
+                string czslovo = Console.ReadLine();
+                Console.Write("Napiš anglický překlad: ");
+                string angslovo = Console.ReadLine();
+
+                //Řešení, jestli dané slovo již ve slovníku není!
+                if (slovnik.ContainsKey(czslovo)) {
+                    Console.WriteLine("Dané slovo, již ve slovníku je. Chceš ho změnit?");
+                    Console.WriteLine($"Verze stará {slovnik[czslovo]} a Nová verze: {angslovo}");
+                    Console.WriteLine("Pokud chceš změnu napiš A, pokud ne, tak N");
+                    string roz = Console.ReadLine();
+                    if (roz == "A")
+                    {
+                        slovnik.Remove(czslovo);
+                        slovnik.Add(czslovo, angslovo);
+                    }
+                } else
+                {
+                    slovnik.Add(czslovo, angslovo);
+                }
+
+                //Pro kontrolu výpis z slovníku
+                foreach (KeyValuePair<string, string> slov in slovnik)
+                {
+                    Console.WriteLine($"{slov.Key} má překlad: {slov.Value}");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
